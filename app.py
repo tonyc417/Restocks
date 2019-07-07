692e5d0cb3a7fecc834c608c282060f704d5ca11
unknown <tonycruz417@gmail.com>
2019-07-07 17:55:38 -0500

    twitter like bot now works

--- app.py
+++ app.py
@@ -18,6 +18,27 @@ class TwitterBot:
         password.clear()
         email.send_keys(self.username)
         password.send_keys(self.password)
+        password.send_keys(Keys.RETURN)
+        time.sleep(3)
 
-ed = TwitterBot('', '') #your email and pass
+    def like_tweet(self,hashtag):
+        bot = self.bot
+        bot.get('https://twitter.com/search?q=' + hashtag +'&src=typd&lang=en')
+        time.sleep(3)
+        for i in range(1,3):
+            bot.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+            time.sleep(2)
+            tweets = bot.find_elements_by_class_name('tweet')
+            links = [elem.get_attribute('data-permalink-path') for elem in tweets]
+            for link in links:
+                bot.get('https://twitter.com/' + link)
+                try:
+                    bot.find_element_by_class_name('HeartAnimation').click()
+                    time.sleep(10)
+                except Exception as ex:
+                    time.sleep(60)
+
+
+ed = TwitterBot('Restocks11', 'Subiegang')
 ed.login()
+ed.like_tweet('stocks')
